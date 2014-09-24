9847c0726d12dcec8b283c5593dc553a6ef1976a
Michal Zdrojewski <code@strayobject.co.uk>
2014-09-24 13:46:31 +0100

    created a vanilla php script and renamed the old one as -phpppa

--- apache-phpppa.sh
+++ apache-phpppa.sh
@@ -10,14 +10,15 @@ echo ">>> Installing Base Items"
  
 # Install base items
 # python-software-properties required to fix missing add-apt-repository
-#sudo apt-get install -y python-software-properties
+sudo apt-get install -y python-software-properties
  
 echo ">>> Adding PPA's and Installing Server Items"
  
 # Add repo for latest PHP
+sudo add-apt-repository -y ppa:ondrej/php5
  
 # Update Again
-#sudo apt-get update
+sudo apt-get update
  
 # Install the Rest
 sudo apt-get install -y curl php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt
@@ -27,4 +28,3 @@ sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.i
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
  
 sudo service apache2 restart
-
