df768f44700a722d0b54525780df68c9731406c1
Michal Zdrojewski <code@strayobject.co.uk>
2014-09-29 15:09:27 +0100

    mizzenlite, glasgowphp and plainmotif on one vm

--- apache-config-mizzenlite.sh
+++ apache-config-mizzenlite.sh
@@ -5,7 +5,7 @@ echo ">>> Configuring Server"
 sudo echo "<VirtualHost *:80>
     DocumentRoot \"/var/www/mizzenlite\"
     ServerName dev.mizzenlite.net
-    ErrorLog \"/var/log/apache2/mizzenlite-error.log\"
+    ErrorLog \"/var/log/apache2/error-mizzenlite.log\"
     <Directory /var/www/mizzenlite>
         <IfModule mod_rewrite.c>
             RewriteEngine on
@@ -19,9 +19,53 @@ sudo echo "<VirtualHost *:80>
 
 </VirtualHost>" >> /etc/apache2/sites-available/mizzenlite.conf
 
+sudo echo "<VirtualHost *:80>
+    DocumentRoot \"/var/www/plainmotif\"
+    ServerName dev.plainmotif.net
+    ErrorLog \"/var/log/apache2/error-plainmotif.log\"
+    <Directory /var/www/plainmotif>
+        <IfModule mod_rewrite.c>
+            RewriteEngine on
+#            RewriteRule  (.*) public/index.php [L]
+        </IfModule>
+
+        # Prevent file browsing
+        Options -Indexes
+        AllowOverride all
+    </Directory>
+
+</VirtualHost>" >> /etc/apache2/sites-available/plainmotif.conf
+
+sudo echo "<VirtualHost *:80>
+    DocumentRoot \"/var/www/glasgowphp\"
+    ServerName dev.glasgowphp.net
+    ErrorLog \"/var/log/apache2/error-glasgowphp.log\"
+    <Directory /var/www/glasgowphp>
+        <IfModule mod_rewrite.c>
+            RewriteEngine on
+#            RewriteRule  (.*) public/index.php [L]
+        </IfModule>
+
+        # Prevent file browsing
+        Options -Indexes
+        AllowOverride all
+    </Directory>
+
+</VirtualHost>" >> /etc/apache2/sites-available/glasgowphp.conf
+
+if [ ! -d /home/vagrant/www/glasgowphp ];
+then
+    mkdir /home/vagrant/www/glasgowphp
+fi
+
 if [ ! -d /home/vagrant/www/mizzenlite ];
 then
     mkdir /home/vagrant/www/mizzenlite
+fi
+
+if [ ! -d /home/vagrant/www/plainmotif ];
+then
+    mkdir /home/vagrant/www/plainmotif
 fi
 
 sudo a2dissite 000-default
@@ -31,5 +75,7 @@ sudo ln -s /home/vagrant/www /var/www
 
 sudo a2enmod rewrite
 sudo a2ensite mizzenlite
+sudo a2ensite plainmotif 
+sudo a2ensite glasgowphp
 
 sudo service apache2 restart
